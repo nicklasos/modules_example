34d0fb8152fd1e948d276477ca8f8a3ecde4c07f
Nicklasos <nicklasos@gmail.com>
2024-01-29 17:03:47 +0200

    Add routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,18 +2,31 @@
 
 declare(strict_types=1);
 
+use App\Modules\Student\Controllers\StudentsListController;
 use App\Modules\Teachers\Controllers\TeachersListController;
 use App\Modules\Teachers\Controllers\TeachersUpdateController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Teachers
+| Teachers module
 |--------------------------------------------------------------------------
 */
-Route::group(['prefix' => 'teachers'], function () {
+Route::group(['prefix' => 'teacher'], function () {
 
     Route::get('/', TeachersListController::class);
     Route::post('/', TeachersUpdateController::class);
+
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Student module
+|--------------------------------------------------------------------------
+*/
+Route::group(['prefix' => 'student'], function () {
+
+    Route::get('/', StudentsListController::class);
 
 });
